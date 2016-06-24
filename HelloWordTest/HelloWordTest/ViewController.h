0f4cf252e50c2236dce61117b61bdded979beee5
刘星星 <lxxwork0827@163.com>
2016-06-24 18:56:51 +0800

    测试提交Git
    第一次测试添加东西

---
+++ HelloWordTest/HelloWordTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWordTest
+//
+//  Created by 刘星星 on 16/6/24.
+//  Copyright © 2016年 szph-software04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWordTest/HelloWordTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWordTest
+//
+//  Created by 刘星星 on 16/6/24.
+//  Copyright © 2016年 szph-software04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
